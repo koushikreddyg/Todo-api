39dbc2a58f790e362b4fb347b33da2b2507a4dfb
koushikui <koushikreddygunapati@gmail.com>
2018-03-18 18:48:32 -0400

    Setting up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,9 @@
 var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+if(process.env.NODE_ENV==='production'){
+  mongoose.connect('mongodb://koushik:password@ds211029.mlab.com:11029/practicemongo')
+}else{
+  mongoose.connect('mongodb://localhost:27017/TodoApp');
+}
+
 module.exports={mongoose};
