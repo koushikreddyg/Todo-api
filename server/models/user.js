f0877fde01b6ff50b6a3a68ef4af36860258fc1e
koushikui <koushikreddygunapati@gmail.com>
2018-03-17 23:16:50 -0400

    added post todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,10 @@
+var mongoose=require('mongoose');
+var User=mongoose.model('User',{
+  email:{
+    required: true,
+    trim: true,
+    type: String,
+    minlength: 1,
+  }
+});
+module.exports={User}
