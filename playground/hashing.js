83dc1030a671c52024321ba308149284c60f6d3b
koushikui <koushikreddygunapati@gmail.com>
2018-03-20 15:53:47 -0400

    added a hash for the user

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,28 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
-var data={
-  id: 10
-};
-var token =jwt.sign(data,'123abc');
-console.log(token)
-var decoded=jwt.verify(token, '123abc');
-console.log('decoded ', decoded)
+const bcrypt=require('bcryptjs');
+var password='123abc!';
+// bcrypt.genSalt(10,(err,salt)=>{
+//   bcrypt.hash(password,salt,(err, hash)=>{
+//     console.log(hash);
+//   })
+// })
+
+var hashedPassword='$2a$10$CKppniQ4BtBwR2vLaLFmzu7JGHiyCdHEzrmvUkhXax1CE5SS6FJje';
+bcrypt.compare(password,hashedPassword, (err,res)=>{
+  console.log(res)
+})
+
+
+
+
+// var data={
+//   id: 10
+// };
+// var token =jwt.sign(data,'123abc');
+// console.log(token)
+// var decoded=jwt.verify(token, '123abc');
+// console.log('decoded ', decoded)
 
 
 
